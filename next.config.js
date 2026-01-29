b359c3e121af0da58ab7e476107719e0784c7511
TakundaMabukwa <mabukwatakunda@gmail.com>
2026-01-29 14:33:41 +0200

    remove browser caching

--- next.config.js
+++ next.config.js
@@ -19,6 +19,15 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+      {
         source: '/sw.js',
         headers: [
           {
